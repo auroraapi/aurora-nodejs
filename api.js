e214ce23a519788cc8c79cbcce7f4695c11ac73b
Anbo Wei <anbo_wei@outlook.com>
2018-03-12 04:39:23 -0700

    awei: Code quality pass for api.js
    api.js changes
    - function(){} -> function() {}
    - JSDoc comments added for functions. 
    - Alphabetized imports
    - Ending semicolons verified.
    
    Note: audio.js still requires heavy modification, so documentation for that will be done in parallel with that modification.

--- api.js
+++ api.js
@@ -1,9 +1,13 @@
 'use strict';
 
+/**
+ * @file Creates wrappers for the Aurora API calls and provides a promise-based framework to interact with the result. This file uses ./audio.js's AudioFile class extensively and does not deal with the high level objects in index.js
+ */
+
+const axios = require('axios');
 let https = require('https');
 https.post = require('https-post');
-const fs = require('fs');
-const axios = require('axios');
+
 const AudioFile = require('./audio');
 
 const BASE_URL = "https://api.auroraapi.com";
@@ -18,16 +22,21 @@ const INTERPRET_PATH = '/v1/interpret/';
 const CONTENT_TYPE = "Content-Type";
 const WAV_MIMETYPE = "audio/wav";
 
-module.exports.getHeaders = function(){
+module.exports.getHeaders = function() {
 	return {
 		"X-Application-ID": global.aurora.store['appId'],
 		"X-Application-Token": global.aurora.store['appToken'],
 		"X-Device-ID": global.aurora.store['deviceId']
-	}
+	};
 }
 
-// Returns a promise with the resulting audio file. 
-module.exports.getTTS = function(text){
+/**
+ * Using Aurora's network API calls, converts the input text to a promise
+ * for an AudioFile.
+ * @param {string} text - The text to be converted into speech.
+ * @return {Promise<AudioFile>} - A promise to obtain an AudioFile.
+ */
+module.exports.getTTS = function(text) {
 	let headers = this.getHeaders();
 	let instance = axios.create({
 		baseURL: BASE_URL,
@@ -43,16 +52,23 @@ module.exports.getTTS = function(text){
 	return instance.get(TTS_URL)
 	.then((httpReponse) => {
 		return AudioFile.createFromStream(httpReponse.data);
-	})
-	.catch((error) => {
-		throw new Error(error);
 	});
 }
 
+/**
+ * @typedef {Object} InterpretObject
+ * @property {string} text - The input text to be interpreted.
+ * @property {string} intent - A one word categorization denoting the intent of the text. 
+ * @property {Object} entities - An object containing other relevant properties given in key-value pairs. 
+ */
 
-// return promise to get json from API
-// TODO: Return the json
-module.exports.getInterpret = function(text){
+/**
+ * Using Aurora's network API calls, converts the input text to a promise
+ * for interpretation of that text.
+ * @param {string} text - The text to be interpreted.
+ * @return {Promise<InterpretObject>} - A promise to obtain interpretation data.
+ */
+module.exports.getInterpret = function(text) {
 	let headers = this.getHeaders();
 	let instance = axios.create({
 		baseURL: BASE_URL,
@@ -71,7 +87,17 @@ module.exports.getInterpret = function(text){
 	});
 }
 
-// Return a promise to get json from an AudioFile.
+/**
+ * @typedef {Object} TextTranscript
+ * @property {string} transcript - A transcript of input audio.
+ */
+
+/**
+ * Using Aurora's network API calls, converts the input AudioFile to a promise
+ * for an AudioFile.
+ * @param {AudioFile} audio - The audio to be converted into text.
+ * @return {Promise<TextTranscript>} - A promise to obtain json data transcribing the data.
+ */
 module.exports.getSTT = function(audio) {
 	let headers = this.getHeaders();
 	headers[CONTENT_TYPE] = WAV_MIMETYPE;
