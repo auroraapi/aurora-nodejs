39958f488dd26a8156bdb04119ed37b0523c4eb0
Anbo Wei <anbo_wei@outlook.com>
2018-03-12 06:39:39 -0700

    awei: Audio.js documentation
    test.js
    - removed some inconsistent timeouts
    
    audio.js
    - Added jsdoc documentation
    - Modified getWavPath() to use unique filenames
    
    TODO: 
    - Buffer processing

--- audio.js
+++ audio.js
@@ -15,7 +15,27 @@ const RATE = 44100;
 const WAV_HEADER_SIZE = 44;
 const WAV_FORMAT_TAG = ".wav";
 
+/**
+ * Aurora's representation for a chunk of audio. Contains methods for the basic
+ * processing, playing and recording, and file management of said audio. 
+ * 
+ * @details
+ * Since surprisingly few audio processing libraries exist for npm, this representation 
+ * uses a streamable buffer in order to store the binary data related to the .wav file. 
+ * Such files are expected to be generated using naudiodon, a node binding for PortAudio.
+ *
+ * @typedef {Object} AudioFile
+ * @property {Buffer} audio
+ * @property {portAudio.AudioOutput} audioOutput
+ */
+
+ // TODO: Consider making your own buffer.
 module.exports = class AudioFile {
+
+	/**
+	 * Creates an AudioFile object from the input stream-buffer
+	 * @param {Buffer} text - The string to be stored. 
+	 */
 	constructor(audio) {
 		// Define this.audio to be a buffer that stores the .wav file data.
 		this.audio = audio;
@@ -24,12 +44,18 @@ module.exports = class AudioFile {
 		this.audioOutput = null;
 	}
 
-	// Removes the metadata from the buffer and returns the resultant buffer.
+	/**
+	 * Removes .wav metadata from the buffer. 
+	 * @return {Buffer} A buffer of the PCM audio data inside the wav data.
+	 */
 	wavWithoutMetadata() {
 		return Buffer.from(this.audio.buffer, WAV_HEADER_SIZE);
 	}
 
-	// Stores the data contained in this object to [fname].wav.
+	/**
+	 * Stores the data contained in this object to [fname].wav. 
+	 * @param {string} fname - The name of the file to write to with a '.wav' appended to the end of the input.
+	 */
 	writeToFile(fname) {
 		let audioReadStream = new streamBuffers.ReadableStreamBuffer();
 		let endFile = fs.createWriteStream(fname + WAV_FORMAT_TAG);
@@ -40,39 +66,73 @@ module.exports = class AudioFile {
 		audioReadStream.stop();
 	}
 
-	// return a stream with the audio data
+	/**
+	 * Return the underlying audio stream.
+	 * @return {Buffer} A buffer of the PCM audio data inside the wav data.
+	 */
 	getWav() {
 		return this.audio;
 	}
 
-	// write the data to wavData.wav, then return the name of the file
-	getWavPath(){
-		const defaultWavName = 'wavData';
+	/**
+	 * Write the data to wavData.wav, then return the name of the file of the format:
+	 * wavData.[yyyy].[MM].[dd].[hh].[mm].[fff].wav
+	 * @return {string} The name of the resulting file with the '.wav' tag included.
+	 */
+	getWavPath() {
+		let defaultWavName = 'wavData';
+		let now = new Date();
+		defaultWavName = defaultWavName +  
+			(now.getFullYear().toString().padStart(4, '0')) + '.' +
+			((now.getMonth() + 1).toString().padStart(2, '0')) + '.' +
+			(now.getDate().toString().padStart(2, '0')) + '.' +
+			(now.getHours().toString().padStart(2, '0')) + '.' +
+			(now.getMinutes().toString().padStart(2, '0')) + '.' +
+			(now.getMilliseconds().toString().padStart(3, '0'));
 		this.writeToFile(defaultWavName);
 		return defaultWavName + WAV_FORMAT_TAG;
 	}
 
-	// TODO
+	/**
+	 * Pad both sides of the audio with the specified amount of silence in seconds.
+	 * @param {number} - seconds: The amount of seconds to add.
+	 */
 	pad(seconds) {
-
+		this.padLeft(seconds);
+		this.padRight(seconds);
 	}
 
+	/**
+	 * Pad the left side of the audio with the specified amount of silence in seconds.
+	 * @param {number} - seconds: The amount of seconds to add.
+	 */
 	// TODO
-	pad_left(seconds) {
+	padLeft(seconds) {
 
 	}
 
+	/**
+	 * Pad the right side of the audio with the specified amount of silence in seconds.
+	 * @param {number} - seconds: The amount of seconds to add.
+	 */
 	// TODO
 	pad_right(seconds) {
 
 	}
 
+	/**
+	 * Trims extraneous silence at the ends of the audio.
+	 */
+	// TODO
 	// TODO
 	trimSilent() {
 
 	}
 
-	// Starts the playback of the audio stored in this object.
+	/**
+	 * Plays the audio stored in this audio file (if it exists) to the default portAudio
+	 * device. The playback of the audio is asynchronous and can be stopped by calling this.stop().
+	 */
 	play() {
 		if (this.audio) {
 			this.audioOutput = new portAudio.AudioOutput({
@@ -99,13 +159,21 @@ module.exports = class AudioFile {
 		}
 	}
 
-	// Stops the playback of the audio.
+	/**
+	 * If audio output is being played from this.play(), stop it.
+	 */
 	stop() {
 		if (this.audioOutput) this.audioOutput.end();
 	}
 
-	// Starts recording data for the specified amounts of time, then returns a
-	// promise with the new AudioFile as the promise result.
+	/**
+	 * Starts recording data for the specified amount of time from the default audio device, then 
+	 * returns a Promise for the actual AudioFile class. 
+	 * @static
+	 * @param {number} length - The amount of time in seconds to record for. If 0, it will record indefinitely, until the specified amount of silence.
+	 * @param {number} silenceLength - The amount of silence in seconds to allow before stopping. Ignored if length != 0.
+	 * @return {Promise<AudioFile>} - The promise for the proper audio file from the recording. 
+	 */
 	static fromRecording(length = 0, silenceLen = 1.0) {
 		let ai = new portAudio.AudioInput({
 			channelCount: NUM_CHANNELS,
@@ -130,27 +198,43 @@ module.exports = class AudioFile {
 		ai.start();
 
 		return new Promise(function(resolve, reject) {
-			setTimeout(function() {
+			setTimeout(() => {
 				ai.quit();
 				resolve(AudioFile.createFromWavData(ws.getContents()));
 			}, length);
 		});
 	}
 
-	// Creates an AudioFile representation from the data. No callback is needed.
+	/**
+	 * Starts recording data for the specified amount of time from the default audio device, then 
+	 * returns a Promise for the actual AudioFile class. 
+	 * @static
+	 * @param {Buffer} d - A buffer containing wav audio data.
+	 * @return {AudioFile} - A proper audio file. No promise returned. 
+	 */
 	static createFromWavData(d) {
 		return new AudioFile(d);
 	}
 
-	// Reads the audio data from the file, appending the .wav extension to the
-	// input filename. Returns the result as a promise.
+	/**
+	 * Reads the audio data from the file, appending the .wav extension to the
+	 * input filename. Returns the result as a promise.
+	 * @static
+	 * @param {string} f - A filename. [filename].wav will be polled for .wav data.
+	 * @return {Promise<AudioFile>} - A promise for an AudioFile. 
+	 */
 	static createFromFile(f) {
 		let readFile = fs.createReadStream(f + WAV_FORMAT_TAG);
 		return AudioFile.createFromStream(readFile);
 	}
 
-	// Reads the audio data from the stream. Returns a promise that will return
-	// the result as the first argument.
+	/**
+	 * Reads the audio data from the stream. Returns a promise that will return
+	 * the result.
+	 * @static
+	 * @param {Stream} s - A pipable stream. It must contain the "end" event.
+	 * @return {Promise<AudioFile>} - A promise for an AudioFile. 
+	 */
 	static createFromStream(s) {
 		let ws = new streamBuffers.WritableStreamBuffer();
 		s.pipe(ws);
