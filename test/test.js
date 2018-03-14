5f563951867fef25815fb4a14ff8c5a7c4c07c00
Anbo Wei <anbo_wei@outlook.com>
2018-03-14 15:35:57 -0700

    awei: api test refactoring to use Mocha's built-in promise framework
    Also cleaned up some styling
    Hoisted setKeys to a separate function

--- test/test.js
+++ test/test.js
@@ -8,12 +8,18 @@ const assert = chai.assert;
 const keys = require('./private').keys;
 
 const aurora = require('../index');
-const api = require('../api');
-const AudioFile = require('../audio');
+const api = aurora.api;
+const AudioFile = aurora.AudioFile;
+
+let setKeys = function() {
+	aurora.setAppId(keys['appId']);
+	aurora.setAppToken(keys['appToken']);
+	aurora.setDeviceId(keys['deviceId']);
+}
 
 /* test aurora API as a whole */
-describe('#aurora', function(){
-	it("exists", function(){
+describe('#aurora', function() {
+	it("exists", function() {
 		expect(aurora).to.exist;
 	});
 
@@ -38,12 +44,12 @@ describe('#aurora', function(){
 
 
 /* test api.js */
-describe('#api', function(){
-	it("exists", function(){
+describe('#api', function() {
+	it("exists", function() {
 		expect(api).to.exist;
 	});
 
-	it("can access stored API data", function(){
+	it("can access stored API data from headers", function() {
 		const testString = "123456";
 		aurora.setAppId(testString);
 		aurora.setAppToken(testString);
@@ -55,78 +61,58 @@ describe('#api', function(){
 		expect(headers["X-Device-ID"]).to.equal(testString);
 	});
 
-	it("converts text to speech", function(done){
+	it("converts text to speech using getTTS()", function() {
+		
+		setKeys();
 
-		const wavName = 'speechResult';
+		const wavName = './test/speechResult';
+		let text = "Hello World!";
 
-		aurora.setAppId(keys['appId']);
-		aurora.setAppToken(keys['appToken']);
-		aurora.setDeviceId(keys['deviceId']);
-		let text = new aurora.Text("hello world");
-
-		text.speech()
-		.then((speech) => {
-			speech.audio.writeToFile(wavName);
+		return api.getTTS(text)
+		.then((audioFile) => {
+			audioFile.writeToFile(wavName);
 			expect(fs.existsSync(wavName + ".wav")).to.be.true;
 			fs.unlinkSync(wavName + ".wav");
-			done();
-		})
-		.catch((error) => {
-			assert.isNotOk(error, "Encountered text to speech error");
-			done(error);
 		});
 	});
 
-	it("interprets text", function(done){
-		aurora.setAppId(keys['appId']);
-		aurora.setAppToken(keys['appToken']);
-		aurora.setDeviceId(keys['deviceId']);
-		let text = new aurora.Text("hello");
-		let resultPromise = text.interpret();
+	it("interprets text using getInterpret()", function() {
+		
+		setKeys();
 
-		resultPromise.then(function(response) {
+		let text = "What's the weather in Los Angles?";
+
+		return api.getInterpret(text)
+		.then((response) => {
 			expect(response.hasOwnProperty('intent')).to.be.true;
 			expect(response.hasOwnProperty('entities')).to.be.true;
-			done();
-		}).catch(function(error){
-			assert.isNotOk(error, "Encountered interpret error");
-			done(error);
 		});
 	});
 
-	it("can convert speech to text", function(done){
-		aurora.setAppId(keys['appId']);
-		aurora.setAppToken(keys['appToken']);
-		aurora.setDeviceId(keys['deviceId']);
+	it("can convert speech to text using getSTT()", function() {
+	
+		setKeys();
 
-		const textString = "Hello friends";
-
-		let text = new aurora.Text(textString);
-
-		text.speech()
-		.then((speech) => {
-			return speech.text();
+		return AudioFile.createFromFile("./test/helloFriends")
+		.then((audioFile) => {
+			return api.getSTT(audioFile);
 		})
-		.then((textObject) => {
-			expect(textObject.text).to.be.a('string');
-			expect(textObject.text).to.equal(textString);
-			done();
-		})
-		.catch((error) => {
-			assert.isNotOk(error, "Encountered speech to text error");
-			done(error);
+		.then((textTranscript) => {
+			console.log(textTranscript);
+			expect(textTranscript.transcript).to.be.a('string');
 		});
 	});
 });
 
 
+
 /* test audio.js */
-describe('#audio', function(){
-	it("exists", function(){
+describe('#audio', function() {
+	it("exists", function() {
 		expect(AudioFile).to.exist;
 	});
 
-	it("fromRecording and playback", function(done){
+	it("fromRecording and playback", function(done) {
 		AudioFile.fromRecording(1000)
 		.then(function(resultingAudioFile) {
 			resultingAudioFile.play();
@@ -135,9 +121,9 @@ describe('#audio', function(){
 	});
 
 
-	it('converts audio data to .wav file', function(done){
+	it('converts audio data to .wav file', function(done) {
 		expect(fs.existsSync('test.wav')).to.be.false;
-		AudioFile.fromRecording(1000).then(function(resultingAudioFile){
+		AudioFile.fromRecording(1000).then(function(resultingAudioFile) {
 			resultingAudioFile.writeToFile('test'); // no .wav tag
 			expect(fs.existsSync('test.wav')).to.be.true;
 
@@ -145,8 +131,8 @@ describe('#audio', function(){
 			expect(fs.existsSync(path)).to.be.true;
 
 			// clean up
-			fs.unlink('test.wav', function(){
-				fs.unlink(path, function(){
+			fs.unlink('test.wav', function() {
+				fs.unlink(path, function() {
 					done();
 				});
 			});
