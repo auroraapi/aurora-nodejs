91385b45f8232c7be4738dee17c4279ce33930cd
Anbo Wei <anbo_wei@outlook.com>
2018-03-15 03:24:56 -0700

    awei: Audio.js Stream-related rewrite
    writeToFile
    - writeToFile now operates asynchronously. A synchronous version writeToFileSync was added.
    
    getWavPath
    - Now operates synchronously 
    
    trimSilence
    - Added wav.js arguments for more flexibility. This is not a functional change. 
    
    play()
    - Rewritten with same functionality with the principle that: 
       - Handlers for readstream error, writestream error, readstream end, and writestream finish must be given.
    
    stop()
    - Also rewritten to maximize code reuse through the stream events.
    
    setUpInputAndWriteStreams()
    - Both fixed and variable length recording have similar stream setup requirements, so they've been hoisted to a private function. 
       - Handlers for readstream error, writestream error, readstream end, and writestream finish written.
    
    recordFixedLength(), recordSilenceAware()
    - fromRecording was getting too long.
    
    createFromStream() 
    - Rewritten. 
       - Handlers for readstream error, writestream error, readstream end, and writestream finish written.

--- audio.js
+++ audio.js
@@ -38,8 +38,11 @@ module.exports = class AudioFile {
 		if (WavBuffer.isWavBuffer(audio)) {
 			this.audio = audio;
 		}
-		else {
+		else if (Buffer.isBuffer(audio)) {
 			this.audio = new WavBuffer(audio);
+		}
+		else {
+			throw "Input is not a buffer!";
 		}
 
 		// An naudiodon output. This is stored so we can stop it later if need be.
@@ -55,17 +58,45 @@ module.exports = class AudioFile {
 	}
 
 	/**
+	 * A callback that takes 1 argument in case of error.
+	 * @callback errorCallback
+	 * @param {Error} error
+	 */
+
+	/**
 	 * Stores the data contained in this object to [fname].wav. 
 	 * @param {string} fname - The name of the file to write to with a '.wav' appended to the end of the input.
+	 * @param {Object} [options] - Same format as fs's writeFile options.
+	 * @param {errorCallback} [callback] - A callback that takes 1 argument in case there is an error. Defaults to throwing.
 	 */
-	writeToFile(fname) {
-		let audioReadStream = new streamBuffers.ReadableStreamBuffer();
-		let endFile = fs.createWriteStream(fname + WAV_FORMAT_TAG);
+	writeToFile(fname, options, callback) {
+		if (!options) {
+			options = {
+				mode: 0o666,
+				flag: 'w'
+			};
+		}
+		if (!callback) {
+			callback = function(error) {
+				if (error) throw error;
+			};
+		}
+		fs.writeFile(fname + WAV_FORMAT_TAG, this.getWav(), options, callback);
+	}
 
-		audioReadStream.pipe(endFile);
-
-		audioReadStream.put(this.getWav());
-		audioReadStream.stop();
+	/**
+	 * Stores the data contained in this object to [fname].wav. 
+	 * @param {string} fname - The name of the file to write to with a '.wav' appended to the end of the input.
+	 * @param {Object} [options] - Same format as fs's writeFile options.
+	 */
+	writeToFileSync(fname, options) {
+		if (!options) {
+			options = {
+				mode: 0o666,
+				flag: 'w'
+			};
+		}
+		fs.writeFileSync(fname + WAV_FORMAT_TAG, this.getWav(), options);
 	}
 
 	/**
@@ -91,7 +122,7 @@ module.exports = class AudioFile {
 			(now.getHours().toString().padStart(2, '0')) + '.' +
 			(now.getMinutes().toString().padStart(2, '0')) + '.' +
 			(now.getMilliseconds().toString().padStart(3, '0'));
-		this.writeToFile(defaultWavName);
+		this.writeToFileSync(defaultWavName);
 		return defaultWavName + WAV_FORMAT_TAG;
 	}
 
@@ -121,18 +152,11 @@ module.exports = class AudioFile {
 
 	/**
 	 * Trims extraneous silence in the audio.
+ 	 * @param {number} [silenceThreshold] - The threshold for silence. Defaults to 1/16.
+	 * @param {number} [blockSeconds] - The number of seconds to consider as a single block. Defaults to 1 second. 
 	 */
-	trimSilent() {
-		this.audio.trimSilence();
-	}
-
-	/**
-	 * Cleans up the audio output. 
-	 * @private
-	 */
-	cleanupOutput() {
-		this.audioOutput.end();
-		this.audioOutput = null;
+	trimSilent(silenceThreshold=1/16, blockSeconds=1) {
+		this.audio.trimSilence(silenceThreshold, blockSeconds);
 	}
 
 	/**
@@ -141,39 +165,57 @@ module.exports = class AudioFile {
 	 * @return {AudioFile} - This audio file that resolves when it's done playing.
 	 */
 	play() {
-		return new Promise((resolve, reject) => {
-			if (this.audio) {
-				this.audioOutput = new portAudio.AudioOutput({
-					channelCount: NUM_CHANNELS,
-					sampleFormat: FORMAT,
-					sampleRate: RATE,
-					deviceId: -1 // default device
-				});
+		let currentWavBuffer = this;
 
-				let readableStream = new streamBuffers.ReadableStreamBuffer();
-
-				// On error, cut the stream.
-				this.audioOutput.on('error', (err) => {
-					console.error(err);
-					this.cleanupOutput();
-					reject(err);
-				});
-
-				// close output stream at end of read stream
-				readableStream.on('end', () => { 
-					this.cleanupOutput();
-					resolve(this);
-				});
-
-				// Trim the first WAV_HEADER_SIZE bytes to avoid playing metadata.
-				readableStream.put(this.wavWithoutMetadata());
-				readableStream.pipe(this.audioOutput);
-				this.audioOutput.start();
-				readableStream.stop();
+		return new Promise(function(resolve, reject) {
+			// If you're already playing, immediately reject.
+			if (currentWavBuffer.audioOutput) {
+				reject(new Error("Already playing something"));
 			}
-			else {
-				reject(new Error("Nothing to play!"));
-			}
+
+			// Load the data to be streamed.
+			let readBuffer = new streamBuffers.ReadableStreamBuffer();
+			readBuffer.put(currentWavBuffer.getWav());
+			readBuffer.stop();
+
+			// Create the audio output stream.
+			currentWavBuffer.audioOutput = new portAudio.AudioOutput({
+				channelCount: NUM_CHANNELS,
+				sampleFormat: FORMAT,
+				sampleRate: RATE,
+				deviceId: -1 // default
+			});
+
+
+
+			// On error, stop the audio output and reject.
+			let rejected = false;
+			let onError = function(error) {
+				console.error(error);
+				rejected = true;
+				reject(error);
+				currentWavBuffer.audioOutput.end();
+			};
+			readBuffer.on('error', onError);
+			currentWavBuffer.audioOutput.on('error', onError);
+
+			// When we're done reading from the buffer, end the output.
+			readBuffer.on("end", () => {
+				currentWavBuffer.audioOutput.end();
+			})
+
+			// On finish, delete the audioOutput and resolve with 
+			// the audio buffer if it hasn't been rejected.
+			currentWavBuffer.audioOutput.on('finish', () => {
+				currentWavBuffer.audioOutput = null;
+				if (!rejected) {
+					resolve(currentWavBuffer);
+				}
+			});
+
+			// Start piping. 
+			readBuffer.pipe(currentWavBuffer.audioOutput);
+			currentWavBuffer.audioOutput.start();
 		});
 	}
 
@@ -182,8 +224,171 @@ module.exports = class AudioFile {
 	 */
 	stop() {
 		if (this.audioOutput) {
-			this.cleanupOutput();
+			this.audioOutput.end();
+			this.audioOutput = null;
 		}
+	}
+
+	/**
+	 * A private helper function to help set up the input and output streams.
+	 * @static
+	 * @param {Function} resolve - Promise handlers.
+	 * @param {Function} reject - Promise handlers.
+	 * @return {Object} - The resulting streams, stored at audioInput and writeStream
+	 * @private
+	 */
+	static setUpInputAndWriteStreams(resolve, reject) {
+		// Load the audio input source.
+		let audioInput = new portAudio.AudioInput({
+			channelCount: NUM_CHANNELS,
+			sampleFormat: FORMAT,
+			sampleRate: RATE,
+			deviceId: -1 // default
+		});
+
+		// Load a buffer to store the input.
+		let writeStream = new streamBuffers.WritableStreamBuffer();
+		// Events to handle: 
+		// readstream error
+		// writestream error
+		// readstream end
+		// writestream finish
+		// Start piping. 
+
+
+		// On error, stop the audio output and reject.
+		let rejected = false;
+		let onError = function(error) {
+			console.error(error);
+			rejected = true;
+			reject(error);
+			// Triggers readstream end
+			audioInput.quit();
+			writeStream.end();
+		};
+		audioInput.on('error', onError);
+		writeStream.on('error', onError);
+
+		// When we're done reading from the mic, end the writeStream.
+		audioInput.on('end', () => {
+			writeStream.end();
+		});
+
+		// On finish, if you hadn't rejected the output, return a new 
+		// AudioBuffer with the data in writeStream.
+		writeStream.on('finish', () => {
+			if (!rejected) {
+				let pcmData = writeStream.getContents();
+				if (!pcmData) {
+					reject(new Error("Problem listening to data."));
+				}
+				else {
+					let recordedBuffer = WavBuffer.generateWavFromPCM(pcmData, {
+						numChannels: NUM_CHANNELS,
+						sampleRate: RATE,
+						bitsPerSample: FORMAT
+					});
+					resolve(AudioFile.createFromWavData(recordedBuffer));
+				}
+			}
+		});
+
+		return {
+			audioInput: audioInput,
+			writeStream: writeStream
+		}
+	}
+
+
+	/**
+	 * A private helper function to help split the task of recording, since fromRecording grew too large.
+	 * @static
+	 * @param {number} length - The amount of time in seconds to record for. 
+	 * @return {Promise<AudioFile>} - The promise for the proper audio file from the recording. 
+	 * @private
+	 */
+	static recordFixedLength(length = 0) {
+		return new Promise(function(resolve, reject) {
+			// Set up streams 
+			// Implicitly sets readstream and writestream errors, readstream end, writestream finish.
+			// Also implicitly resolve.
+			let streams = AudioFile.setUpInputAndWriteStreams(resolve, reject);
+			let audioInput = streams.audioInput;
+			let writeStream = streams.writeStream;
+
+			// Start piping.
+			audioInput.start();
+			audioInput.pipe(writeStream);
+
+			// Timeout in length time.
+			setTimeout(() => {
+				audioInput.quit();
+			}, length * 1000);
+		});
+	}
+
+	/**
+	 * A private helper function to help split the task of recording.
+	 * @static
+	 * @param {number} silenceLength - The amount of silence in seconds to allow before stopping.
+	 * @return {Promise<AudioFile>} - The promise for the proper audio file from the recording. 
+	 * @private
+	 */
+	static recordSilenceAware(silenceLen = 1.0) {
+		return new Promise(function(resolve, reject) {
+			// Set up streams 
+			// Implicitly sets readstream and writestream errors, readstream end, writestream finish.
+			// Also implicitly resolve.
+			let streams = AudioFile.setUpInputAndWriteStreams(resolve, reject);
+			let audioInput = streams.audioInput;
+			let writeStream = streams.writeStream;
+
+			let bytesPerSubsample = FORMAT / 8;
+			let subsampleSilenceTarget = RATE * NUM_CHANNELS * silenceLen;
+			let subsamplesInSilence = 0;
+
+			// Whenever we receive a data chunk, note how long it is. Test if it is silence, 
+			// then, if so, add the time corresponding to the number of samples spent in silence
+			// to samplesInSilence. In either case, write to the data chunk.
+			audioInput.on('data', (dataChunk) => {
+				writeStream.write(dataChunk);
+				
+				// Loop through every subsample in the data chunk to find the one with the max amplitude.
+				let numSubsamplesInChunk = dataChunk.length / bytesPerSubsample;
+				let maxAmplitude = 0;
+				// For each subsample in the dataChunk
+				for (let currentSubsample = 0; currentSubsample < numSubsamplesInChunk; currentSubsample++) {
+					// Get the current subsample
+					let currentSubsampleByteOffset = currentSubsample * bytesPerSubsample;
+					let currentSubsampleValue = dataChunk.readIntLE(currentSubsampleByteOffset, bytesPerSubsample);
+
+					// Save if greater than max.
+					let currentSubsampleAmplitude = Math.abs(currentSubsampleValue);
+					if (currentSubsampleAmplitude > maxAmplitude) {
+						maxAmplitude = currentSubsampleAmplitude;
+					}
+				}
+
+				// If the maximum amplitude of the sample is greater than our silence threshold, 
+				// reset the subsamples we've spent in silence. 
+				if (maxAmplitude >= SILENT_THRESH) {
+					console.log("Active");
+					subsamplesInSilence = 0;
+				}
+				// Otherwise, increment the amount of silence we've encountered.
+				else {
+					console.log("Inactive");
+					subsamplesInSilence += numSubsamplesInChunk;
+				}
+				// If we've spent enough time in silence, finish recording.
+				if (subsamplesInSilence >= subsampleSilenceTarget) {
+					audioInput.quit();
+				}
+			});
+
+			// Start recording.
+			audioInput.start();
+		});
 	}
 
 	/**
@@ -194,107 +399,12 @@ module.exports = class AudioFile {
 	 * @param {number} silenceLength - The amount of silence in seconds to allow before stopping. Ignored if length != 0.
 	 * @return {Promise<AudioFile>} - The promise for the proper audio file from the recording. 
 	 */
-	static fromRecording(length = 0, silenceLen = 1.0) {
-		let ai = new portAudio.AudioInput({
-			channelCount: NUM_CHANNELS,
-			sampleFormat: FORMAT,
-			sampleRate: RATE,
-			deviceId: -1 // default device
-		});
-		
-		// create write stream to write out to raw audio file
-		let ws = new streamBuffers.WritableStreamBuffer();
-
-		let cleanupStreams = function() {
-			ai.quit();
-			ws.end();
-		};
-
+	static fromRecording(length = 0, silenceLength = 1.0) {
 		if (length != 0) {
-			// Pipe for a finite amount of time and make a new wav from the data.
-			ai.pipe(ws);
-			ai.start();
-			
-			return new Promise(function(resolve, reject) {
-				ai.on('error', (err) => {
-					console.error(err);
-					cleanupStreams();
-					reject(err);
-				});
-
-				setTimeout(() => {
-					cleanupStreams();
-					let pcmData = ws.getContents();
-					if (!pcmData) {
-						reject(new Error("Recording error."));
-					}
-					let recordedBuffer = WavBuffer.generateWavFromPCM(pcmData, {
-						numChannels: NUM_CHANNELS,
-						sampleRate: RATE,
-						bitsPerSample: FORMAT
-					});
-					resolve(AudioFile.createFromWavData(recordedBuffer));
-				}, length * 1000);
-			});
+			return AudioFile.recordFixedLength(length);
 		}
 		else {
-			return new Promise(function(resolve, reject) {
-				let bytesPerSubsample = FORMAT / 8;
-
-				// Start hooking up handlers. 
-				// On error, reject the promise.
-				ai.on('error', (err) => {
-					console.error(err);
-					cleanupStreams();
-					reject(err);
-				});
-
-				let subsampleSilenceTarget = RATE * NUM_CHANNELS * silenceLen;
-				let subsamplesInSilence = 0;
-
-				// Whenever we receive a data chunk, note how long it is. Test if it is silence, 
-				// then, if so, add the time corresponding to the number of samples spent in silence
-				// to samplesInSilence. In either case, write to the data chunk.
-				ai.on('data', (dataChunk) => {
-					ws.write(dataChunk);
-
-					let numSubsamplesInChunk = dataChunk.length / bytesPerSubsample;
-					// Loop through the samples to see if we spent them in silence.
-					let maxAmplitude = 0;
-					for (let currentSubsamplePos = 0; currentSubsamplePos < dataChunk.length; currentSubsamplePos += bytesPerSubsample) {
-						let currentValue = dataChunk.readIntLE(currentSubsamplePos, bytesPerSubsample);
-						if (Math.abs(currentValue) > maxAmplitude) 
-							maxAmplitude = currentValue;
-					}
-
-					// If we spent them in silence, increase subsamplesInSilence.
-					if (maxAmplitude < SILENT_THRESH) {
-						subsamplesInSilence += numSubsamplesInChunk;
-					}
-					// Otherwise, reset the count.
-					else {
-						subsamplesInSilence = 0;
-					}
-					// If we've been in silence long enough, finish listening.
-					if (subsamplesInSilence >= subsampleSilenceTarget) {
-						cleanupStreams();
-					}
-				});
-
-				// When we're done with the recording, end the audio input and resolve the promise.
-				ai.on('end', () => {
-					let recordedBuffer = WavBuffer.generateWavFromPCM(ws.getContents(), {
-						numChannels: NUM_CHANNELS,
-						sampleRate: RATE,
-						bitsPerSample: FORMAT
-					});
-					resolve(AudioFile.createFromWavData(recordedBuffer));
-					ws.end();
-				});
-
-				// Start recording.
-				ai.start();
-			});
+			return AudioFile.recordSilenceAware(silenceLength);
 		}
 	}
 
@@ -322,24 +432,59 @@ module.exports = class AudioFile {
 	}
 
 	/**
-	 * Reads the audio data from the stream. Returns a promise that will return
+	 * Reads the .wav audio data from the stream. Returns a promise that will return
 	 * the result.
 	 * @static
-	 * @param {Stream} s - A pipable stream. It must contain the "end" event.
+	 * @param {Stream} readStream - A read stream.
 	 * @return {Promise<AudioFile>} - A promise for an AudioFile. 
 	 */
-	static createFromStream(s) {
-		let ws = new streamBuffers.WritableStreamBuffer();
-		s.pipe(ws);
+	static createFromStream(readStream) {
+
+		// Events to handle: 
+		// readstream error
+		// writestream error
+		// readstream end
+		// writestream finish
+		// Start piping. 
 
 		return new Promise(function(resolve, reject) {
-			s.on("end", () => {
-				resolve(AudioFile.createFromWavData(ws.getContents()));
+
+			let writeStream = new streamBuffers.WritableStreamBuffer();
+			
+
+			// On error, stop the audio output and reject.
+			let rejected = false;
+			let onError = function(error) {
+				console.error(error);
+				rejected = true;
+				reject(error);
+				// Triggers readstream end
+				writeStream.end();
+			};
+			readStream.on('error', onError);
+			writeStream.on('error', onError);
+
+			// When we're done reading from the mic, end the writeStream.
+			readStream.on('end', () => {
+				writeStream.end();
 			});
 
-			s.on("error", (error) => {
-				reject(error);
+			// On finish, if you hadn't rejected the output, return a new 
+			// AudioBuffer with the data in writeStream.
+			writeStream.on('finish', () => {
+				if (!rejected) {
+					let wavData = writeStream.getContents();
+					if (!pcmData) {
+						reject(new Error("Problem with streaming of .wav data."));
+					}
+					else {
+						resolve(AudioFile.createFromWavData(wavData));
+					}
+				}
 			});
+
+			// Start piping.
+			s.pipe(ws);
 		});
 	}
 };
